9bd7d59c29226bd1bf91f3f970c4213558f790be
zestymec <m.umer394aziz@gmail.com>
2026-04-25 16:48:50 +0500

    AppWriteContext created

---
+++ src/appwrite/AppWriteContext.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AppWriteContext = () => {
+  return (
+    <View>
+      <Text>AppWriteContext</Text>
+    </View>
+  )
+}
+
+export default AppWriteContext
+
+const styles = StyleSheet.create({})
